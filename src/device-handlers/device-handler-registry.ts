202905d54fecf4f190941abd76471c9b77be4c49
Olli Martin <oss@ollimart.in>
2026-01-18 17:47:11 +0100

    Skip registering light handler (wip)

--- src/device-handlers/device-handler-registry.ts
+++ src/device-handlers/device-handler-registry.ts
@@ -1,11 +1,14 @@
 import { injectable, registry } from 'tsyringe'
 import { DiTokens } from '../dependency-injection/tokens.js'
 import { GenericHandler } from './generic-handler.js'
-import { LightHandler } from './light-handler.js'
 
 @injectable()
 @registry([
 	{ token: DiTokens.DeviceHandler, useClass: GenericHandler },
-	{ token: DiTokens.DeviceHandler, useClass: LightHandler },
+
+	// The light handler is work in progress that is not fully implemented.
+	// I do not want to delete the code and later dig through the git history to find it again.
+
+	/* { token: DiTokens.DeviceHandler, useClass: LightHandler }, */
 ])
 export class DeviceHandlerRegistry {}
